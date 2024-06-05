6be01538c87ecc93ec653a9da7a67fab099ab9f7
Vladimir Vovk <vladimir@vovk.in>
2024-06-05 14:01:04 +0100

    fix(links): prevent focusing links

--- src/components/terminal/link.tsx
+++ src/components/terminal/link.tsx
@@ -9,8 +9,12 @@ type Props = {
 }
 
 export const Link = ({ text, href, isActive, onFinish }: Props) => {
+  const onMouseDown = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault()
+  }
+
   return (
-    <a className={styles.link} href={href} target="_blank">
+    <a className={styles.link} href={href} target="_blank" onMouseDown={onMouseDown}>
       <Output {...{ text, isActive, onFinish }} />
     </a>
   )
